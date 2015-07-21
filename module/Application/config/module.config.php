9cc5d443a099b02d98102b80124047b4d419d506
Dmytro Borysovskiy <bbbara10@gmail.com>
2015-07-21 15:53:11 +0300

    adding custom 404 error displaying

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -80,12 +80,12 @@ return array(
         'display_not_found_reason' => true,
         'display_exceptions'       => true,
         'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
+        'not_found_template'       => 'error/custom404',
         'exception_template'       => 'error/index',
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/404'               => __DIR__ . '/../view/error/custom404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
